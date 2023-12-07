6a9e5f17861555ad2613f456a81f6afaea4af8bb
Junior Poku <juniorpoku@Juniors-MacBook-Pro.local>
2023-12-07 14:08:09 -0500

    Finsihed index model with associations

--- models/index.js
+++ models/index.js
@@ -5,13 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+//belongstoassociation
+Product.belongsTo(Category);
 // Categories have many Products
-
+//hasmany association
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
+//belongstomany association
+Product.belongsToMany(Tag,{
+  through:{
+    model: ProductTag,
+    foreignKey: 'product_id'
+  }
+})
 
 // Tags belongToMany Products (through ProductTag)
-
+//belongstomany assocation
+Tag.belongsToMany(Product,{
+  through:{
+    model: ProductTag,
+    foreignKey: 'tag_id'
+  }
+})
 module.exports = {
   Product,
   Category,
